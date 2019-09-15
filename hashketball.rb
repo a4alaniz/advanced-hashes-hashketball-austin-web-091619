acd6d837ea8a4fa6aabfadc1c7a2e7f6794c4189
Angel Alanis <a4alaniz@gmail.com>
2019-09-15 02:51:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -201,6 +201,7 @@ def big_shoe_rebounds
   numbs = [] 
   game_hash.each do |team, stats|
       stats[:players].each do |player, data|
+        binding.pry 
         numbs.push(data[:shoe])
       biggest_shoesize = numbs.sort[-1]
     end 
